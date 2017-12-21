808b89582c702f731ff654772cf9c46b1b5d4fd2
evehne <deefaze@hotmail.fr>
2017-12-21 14:40:00 +0100

    Create w3.rtl.common.js

---
+++ dist/rtl/w3.rtl.common.js
@@ -0,0 +1,12 @@
+"use strict";
+
+w3.isObject = function(aRt){ return aRt instanceof Object && r.constructor === Object; };
+
+w3.isArray = function(aRt){ return aRt instanceof Object && r.constructor === Array; };
+
+w3.exec = function(aRt, aFn){ if(w3.isObject(aRt)){ aFn(aRt); } else if(w3.isArray(aRt)) { var i, l=aRet.length; for(i=0; i<l; i++){ aFn(aRt[i]); }} return w3; };
+
+w3.get = function(aSe){ var e=w3.getElements(aSe); if(e.length == 1){ return e[0]; } else { return e; }};
+
+w3.delay = async function(aMs, aFn){ await w3.sleep(aMs); return aFn();}
+w3.sleep = function(aMs){ return new Promise(f => setTimeout(f, aMs)); };
