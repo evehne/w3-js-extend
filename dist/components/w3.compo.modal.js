5f74de3086e5bd413dec199f7293fb5d3beb3bb7
evehne <deefaze@hotmail.fr>
2017-12-23 10:23:21 +0100

    Create w3.compo.modal.js

---
+++ dist/components/w3.compo.modal.js
@@ -0,0 +1,13 @@
+w3.modal = {
+  init : function()
+  {
+    w3.on('[ui-modal-opener]', On.mouse.click, function(){
+      var target = w3.getAttr(this, 'ui-modal-target');
+      w3.show(target);
+    });
+    w3.on('[ui-modal-closer]', On.mouse.click, function(){
+      var target = w3.getAttr(this, 'ui-modal-target');
+      w3.hide(target);
+    });
+  }
+};
